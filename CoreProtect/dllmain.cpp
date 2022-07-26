16a835a34d1305d9b32ff8a1f37eb62b1d515310
KocTu4eK <koctu4ek.koctu4ek@gmail.com>
2022-07-26 16:40:31 +0600

    Очистка проекта

--- CoreProtect/dllmain.cpp
+++ CoreProtect/dllmain.cpp
@@ -1,16 +1,16 @@
 ﻿#include "pch.h"
-#pragma comment(lib, "../LLoaderSDK/Lib/bedrock_server_api.lib")
-#pragma comment(lib, "../LLoaderSDK/Lib/bedrock_server_var.lib")
-#pragma comment(lib, "../LLoaderSDK/Lib/SymDBHelper.lib")
-#pragma comment(lib, "../LLoaderSDK/Lib/LiteLoader.lib")
-#pragma comment(lib, "../LLoaderSDK/Lib/yaml-cpp.lib")
+#pragma comment(lib, "bedrock_server_api.lib")
+#pragma comment(lib, "bedrock_server_var.lib")
+#pragma comment(lib, "SymDBHelper.lib")
+#pragma comment(lib, "LiteLoader.lib")
+#pragma comment(lib, "yaml-cpp.lib")
 
 BOOL APIENTRY DllMain(HMODULE hModule,  DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        LL::registerPlugin("CoreProtect", "Unofficial port of CoreProtect plugin to Minecraft Bedrock", LL::Version(0, 0, 1));
+        LL::registerPlugin("CoreProtect", "Unofficial port of CoreProtect plugin (1.19.10)", LL::Version(0, 0, 1));
 		std::filesystem::create_directory("./plugins/CoreProtect");
 		break;
     case DLL_THREAD_ATTACH:
@@ -21,13 +21,7 @@ BOOL APIENTRY DllMain(HMODULE hModule,  DWORD  ul_reason_for_call, LPVOID lpRese
     return TRUE;
 }
 
-extern void plugin();
-
-extern "C"
+extern "C" _declspec(dllexport) void onPostInit()
 {
-    _declspec(dllexport) void onPostInit()
-    {
-        std::ios::sync_with_stdio(false);
-        plugin();
-    }
+    std::ios::sync_with_stdio(false);
 }
--- CoreProtect/pch.h
+++ CoreProtect/pch.h
@@ -1,13 +1,6 @@
 ﻿#ifndef PCH_H
 #define PCH_H
 
-#include <windows.h>
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <Global.h>
 #include <LLAPI.h>
-#include <yaml-cpp/yaml.h>
-#include <SQLiteCpp/SQLiteCpp.h>
 
 #endif //PCH_H
