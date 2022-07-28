59255e6d2ac23c4051a3e10b92ee91cbc8901216
KocTu4eK <koctu4ek.koctu4ek@gmail.com>
2022-07-28 15:58:02 +0600

    Ловля ошибок и вывод в debug

--- CoreProtect/utility/utility.cpp
+++ CoreProtect/utility/utility.cpp
@@ -3,42 +3,49 @@
 
 void Utility::createConfig()
 {
-	std::ifstream iConfig("./plugins/CoreProtect/config.yml");
-	int configSize = iConfig.tellg();
-	iConfig.close();
-
-	if (!configSize || configSize == -1)
+	try
 	{
-		std::cout << "a";
-		HRSRC hRsrc = FindResource(NULL, (LPCWSTR)CONFIG, L"YAML");
-		if (hRsrc)
+		std::ifstream iConfig("./plugins/CoreProtect/config.yml");
+		int configSize = iConfig.tellg();
+		iConfig.close();
+
+		if (!configSize || configSize == -1)
 		{
-			HGLOBAL hGlob = LoadResource(NULL, hRsrc);
-			if (hGlob)
+			HRSRC hRsrc = FindResource(NULL, (LPCWSTR)CONFIG, L"YAML");
+			if (hRsrc)
 			{
-				BYTE* lpdArray = (BYTE*)LockResource(hGlob);
-				std::string config = (char*)lpdArray;
-				std::ofstream oTemp("./plugins/CoreProtect/.config");
-				oTemp << config;
-				oTemp.close();
+				HGLOBAL hGlob = LoadResource(NULL, hRsrc);
+				if (hGlob)
+				{
+					BYTE* lpdArray = (BYTE*)LockResource(hGlob);
+					std::string config = (char*)lpdArray;
+					std::ofstream oTemp("./plugins/CoreProtect/.config");
+					oTemp << config;
+					oTemp.close();
 
-				std::ifstream iTemp("./plugins/CoreProtect/.config");
-				std::string line;
-				std::ofstream oConfig("./plugins/CoreProtect/config.yml");
-				while (std::getline(iTemp, line)) oConfig << line;
-				iTemp.close();
-				oConfig.close();
+					std::ifstream iTemp("./plugins/CoreProtect/.config");
+					std::string line;
+					std::ofstream oConfig("./plugins/CoreProtect/config.yml");
+					while (std::getline(iTemp, line)) oConfig << line;
+					iTemp.close();
+					oConfig.close();
 
-				std::filesystem::remove("./plugins/CoreProtect/.config");
-				return;
+					std::filesystem::remove("./plugins/CoreProtect/.config");
+					return;
+				}
 			}
 		}
+
+		logger.error << "Config creating error." << logger.endl;
 	}
-	
-	logger.error << "Config creating error." << logger.endl;
+	catch (std::exception ex) { logger.debug << ex.what() << logger.endl; }
 }
 
 void Utility::createLang(const std::string lang)
 {
-	logger.error << "Lang creating error." << logger.endl;
+	try
+	{
+		logger.error << "Lang creating error." << logger.endl;
+	}
+	catch (std::exception ex) { logger.debug << ex.what() << logger.endl; }
 }
